0d804ba9eb9ccb49ca6be69dbb500a5c8c25cd2e
Felipe Miranda Arboleda <Felipe.Miranda@ibm.com>
2024-04-02 17:37:08 -0500

    02.04.2024

--- 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 05-pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGModule } from '../../../prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [],
+  imports: [PrimeNGModule, CommonModule],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
+
+
+  public grossIncome : number = 21500010;
+  public percentage : number = 0.66;
+  public netIncome : number = this.grossIncome * this.percentage;
+  
 
 }
--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PrimeNGModule } from '../../../prime-ng/prime-ng.module';
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PrimeNGModule],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+    public name : string = "Pederson";
+    public gender : 'male'|'female' = 'male';
 
+    invitationMap = {
+      male:"invitarlo",
+      female:"invitarla"
+    }
+
+    changePerson() : void {
+      this.name = "Draculaura";
+      this.gender = "female";
+    }
+
+    //i18nPlural
+
+    public invitados : string[] = ["anuel", "bad bunny", "Arkangel", "Ñengo flow", "Yandel", "Daddy Yankee", "Tokischa", "Doble P", "luigi 21+"];
+
+    invitadosMapping = {
+      "=0":"No hay invitados en la cola de espera.",
+      "=1":"Hay un (1) invitado en la cola de espera.",
+      "other":"Hay # invitados en la cola de espera."
+    }
+
+    deleteInvitado(){
+      this.invitados.splice(0, 1);
+    }
 }
